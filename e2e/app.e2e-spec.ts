7979c547d632f5c97a91c30ec55e8c751bb8e4ef
angular-cli <angular-cli@angular.io>
2016-12-28 09:50:22 +0100

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { RecrepUiPage } from './app.po';
+
+describe('recrep-ui App', function() {
+  let page: RecrepUiPage;
+
+  beforeEach(() => {
+    page = new RecrepUiPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
