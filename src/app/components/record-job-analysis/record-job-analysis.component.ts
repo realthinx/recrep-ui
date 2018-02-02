4481835726e5b1e398e1e3d669e708b9b7726742
ue68754 <daniel.maier@sbb.ch>
2018-02-02 10:40:07 +0100

    Correlation ID added for analysis

--- src/app/components/record-job-analysis/record-job-analysis.component.ts
+++ src/app/components/record-job-analysis/record-job-analysis.component.ts
@@ -103,11 +103,11 @@ export class RecordJobAnalysisComponent implements OnInit {
   }
 
   createUUID = (): string => {
-    var dt = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = (dt + Math.random()*16)%16 | 0;
-      dt = Math.floor(dt/16);
-      return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    let dt = new Date().getTime();
+    const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      const r = (dt + Math.random() * 16) % 16 | 0;
+      dt = Math.floor(dt / 16);
+      return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
     });
 
     return uuid;
